7dd98afa6af3959ad5ab5fbec63c962c1caec889
willbigas <williambmauro@hotmail.com>
2018-09-26 11:05:19 -0300

    Initial commit

---
+++ src/objetos/TerrenoVazio.java
@@ -0,0 +1,22 @@
+package objetos;
+
+/**
+ *
+ * @author William
+ */
+public class TerrenoVazio extends Imovel {
+
+    @Override
+    public void status() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() + "TerrenoVazio{" + '}';
+    }
+    
+    
+    
+
+}
